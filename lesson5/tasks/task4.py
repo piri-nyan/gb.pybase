f83a6a98dbc823d8c7acb2b913c6943b552d0955
piri <hail.piri@gmail.com>
2022-03-26 13:05:22 +0300

    half of task4 done

---
+++ lesson5/tasks/task4.py
@@ -0,0 +1,15 @@
+num2word = {"one": {"ru": "один", "num": 1},
+            "two": {"ru": "два", "num": 2},
+            "three": {"ru": "три", "num": 3},
+            "four": {"ru": "четыре", "num": 4},
+            "five": {"ru": "пять", "num": 5},
+            "six": {"ru": "шесть", "num": 6},
+            "seven": {"ru": "семь", "num": 7},
+            "eight": {"ru": "восемь", "num": 8},
+            "nine": {"ru": "девять", "num": 9},
+            "zero": {"ru": "ноль", "num": 0}}
+
+file = open("file", "rw+", encoding="utf8")
+
+
+
